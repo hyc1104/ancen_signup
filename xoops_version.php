01bdb7b540dda7ca9866fc3580ec906924514a34
Ancen <ancen@mail.clhyc.tw>
2021-10-03 13:48:47 +0800

    3-3-2 新增 TadDataCenter 用的資料表

--- xoops_version.php
+++ xoops_version.php
@@ -36,7 +36,7 @@ $modversion['system_menu'] = 1;
 
 //---模組資料表架構---//
 $modversion['sqlfile']['mysql'] = 'sql/mysql.sql';
-$modversion['tables'] = ['ancen_signup_actions', 'ancen_signup_data'];
+$modversion['tables'] = ['ancen_signup_actions', 'ancen_signup_data', 'ancen_signup_data_center'];
 
 //---後台管理介面設定---//
 $modversion['hasAdmin'] = 1;
