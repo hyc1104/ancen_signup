8a408a9b6ded0ee61fcca7f47ae6d1cdff06ebfa
Ancen <ancen@mail.clhyc.tw>
2021-10-03 13:35:39 +0800

    3-3-1 新增活動報名的資料表

--- xoops_version.php
+++ xoops_version.php
@@ -36,7 +36,7 @@ $modversion['system_menu'] = 1;
 
 //---模組資料表架構---//
 $modversion['sqlfile']['mysql'] = 'sql/mysql.sql';
-$modversion['tables'] = ['ancen_signup_actions'];
+$modversion['tables'] = ['ancen_signup_actions', 'ancen_signup_data'];
 
 //---後台管理介面設定---//
 $modversion['hasAdmin'] = 1;
