8f9de98c994f7ee7c3ffb0105bda40ff3ae394c2
Ancen <ancen@mail.clhyc.tw>
2021-10-11 19:45:13 +0800

    4-2-1 在活動列表顯示報名人數

--- class/Ancen_signup_data.php
+++ class/Ancen_signup_data.php
@@ -48,9 +48,11 @@ class Ancen_signup_data
         $xoopsTpl->assign("token_form", $token_form);
 
         $action = Ancen_signup_actions::get($action_id);
-
+        $action['signup'] = Ancen_signup_data::get_all($action_id);
         if (time() > strtotime($action['end_date'])) {
             redirect_header($_SERVER['PHP_SELF'], 3, "已報名截止，無法再進行報名或修改報名!");
+        } elseif (count($action['signup']) >= $action['number']) {
+            redirect_header($_SERVER['PHP_SELF'], 3, "人數已滿，無法再進行報名!");
         }
 
         $myts = \MyTextSanitizer::getInstance();
