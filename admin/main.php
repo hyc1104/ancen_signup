0e70e932e1e55b99dc7c74a022841c159d108dff
Ancen <ancen@mail.clhyc.tw>
2021-10-18 14:08:59 +0800

    5-3-1 建立後台頁面 main.php

--- admin/main.php
+++ admin/main.php
@@ -1,14 +1,11 @@
 <?php
-// 如「模組目錄」= signup，則「首字大寫模組目錄」= Signup
-// 如「資料表名」= actions，則「模組物件」= Actions
 use Xmf\Request;
-use XoopsModules\首字大寫模組目錄\模組物件;
-
+use XoopsModules\Ancen_signup\Ancen_signup_actions;
 /*-----------引入檔案區--------------*/
-$GLOBALS['xoopsOption']['template_main'] = '模組目錄_admin.tpl';
+$GLOBALS['xoopsOption']['template_main'] = 'ancen_signup_admin.tpl';
 require_once __DIR__ . '/header.php';
 require_once dirname(__DIR__) . '/function.php';
-$_SESSION['模組目錄_adm'] = true;
+$_SESSION['ancen_signup_adm'] = true;
 
 /*-----------變數過濾----------*/
 $op = Request::getString('op');
@@ -17,42 +14,43 @@ $id = Request::getInt('id');
 /*-----------執行動作判斷區----------*/
 switch ($op) {
 
-    //新增表單
-    case '資料表名_create':
-        模組物件::create();
+    //新增活動表單
+    case 'ancen_signup_actions_create':
+        Ancen_signup_actions::create();
         break;
 
-    //新增資料
-    case '資料表名_store':
-        $id = 模組物件::store();
+    //新增活動資料
+    case 'ancen_signup_actions_store':
+        $id = Ancen_signup_actions::store();
         header("location: {$_SERVER['PHP_SELF']}?id=$id");
         exit;
 
     //修改用表單
-    case '資料表名_edit':
-        模組物件::create($id);
-        $op = '資料表名_create';
+    case 'ancen_signup_actions_edit':
+        Ancen_signup_actions::create($id);
+        $op = 'ancen_signup_actions_create';
         break;
 
     //更新資料
-    case '資料表名_update':
-        模組物件::update($id);
+    case 'ancen_signup_actions_update':
+        Ancen_signup_actions::update($id);
         header("location: {$_SERVER['PHP_SELF']}?id=$id");
         exit;
 
     //刪除資料
-    case '資料表名_destroy':
-        模組物件::destroy($id);
-        header("location: {$_SERVER['PHP_SELF']}");
+    case 'ancen_signup_actions_destroy':
+        Ancen_signup_actions::destroy($id);
+        //header("location: {$_SERVER['PHP_SELF']}");
+        redirect_header($_SERVER['PHP_SELF'], 3, "成功刪除活動!");
         exit;
 
     default:
         if (empty($id)) {
-            模組物件::index();
-            $op = '資料表名_index';
+            Ancen_signup_actions::index();
+            $op = 'ancen_signup_actions_index';
         } else {
-            模組物件::show($id);
-            $op = '資料表名_show';
+            Ancen_signup_actions::show($id);
+            $op = 'ancen_signup_actions_show';
         }
         break;
 }
@@ -63,5 +61,5 @@ switch ($op) {
 $xoopsTpl->assign('now_op', $op);
 $xoTheme->addStylesheet('/modules/tadtools/css/font-awesome/css/font-awesome.css');
 $xoTheme->addStylesheet(XOOPS_URL . '/modules/tadtools/css/xoops_adm4.css');
-$xoTheme->addStylesheet(XOOPS_URL . '/modules/模組目錄/css/module.css');
+$xoTheme->addStylesheet(XOOPS_URL . '/modules/ancen_signup/css/module.css');
 require_once __DIR__ . '/footer.php';
