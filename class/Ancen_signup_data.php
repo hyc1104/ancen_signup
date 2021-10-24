0eb859abbe6ffd4d63f50a13922eec2eee043086
Ancen <ancen@mail.clhyc.tw>
2021-10-24 16:16:27 +0800

    6-5-4 讓候補功能生效

--- class/Ancen_signup_data.php
+++ class/Ancen_signup_data.php
@@ -54,7 +54,7 @@ class Ancen_signup_data
             redirect_header($_SERVER['PHP_SELF'], 3, "已報名截止，無法再進行報名或修改報名!");
         } elseif (!$action['enable']) {
             redirect_header($_SERVER['PHP_SELF'], 3, "該報名已關閉，無法再進行報名或修改報名!");
-        } elseif (count($action['signup']) >= $action['number']) {
+        } elseif (count($action['signup']) >= ($action['number'] + $action['candidate'])) {
             redirect_header($_SERVER['PHP_SELF'], 3, "人數已滿，無法再進行報名!");
         }
 
