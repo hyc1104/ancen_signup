45a0afcf44bb5214ec21e2fd1309da7b3200175c
Ancen <ancen@mail.clhyc.tw>
2021-10-30 17:06:30 +0800

    7-2-2 產生 CSV 完整內容

--- csv.php
+++ csv.php
@@ -1,6 +1,7 @@
 <?php
 use Xmf\Request;
 use XoopsModules\Ancen_signup\Ancen_signup_actions;
+use XoopsModules\Ancen_signup\Ancen_signup_data;
 use XoopsModules\Tadtools\TadDataCenter;
 
 require_once __DIR__ . '/header.php';
@@ -10,6 +11,8 @@ if (!$_SESSION['can_add']) {
 }
 
 $id = Request::getInt('id');
+$type = Request::getString('type');
+
 $action = Ancen_signup_actions::get($id);
 
 if ($action['uid'] != $xoopsUser->uid()) {
@@ -26,6 +29,33 @@ $head[] = '報名日期';
 $head[] = '身份';
 
 $csv[] = implode(',', $head);
+
+if ($type == 'signup') {
+    $signup = Ancen_signup_data::get_all($action['id']);
+
+    foreach ($signup as $signup_data) {
+        $item = [];
+        foreach ($signup_data['tdc'] as $user_data) {
+            $item[] = implode('|', $user_data);
+        }
+
+        if ($signup_data['accept'] === '1') {
+            $item[] = '錄取';
+        } elseif ($signup_data['accept'] === '0') {
+            $item[] = '未錄取';
+        } else {
+            $item[] = '尚未設定';
+        }
+
+        $item[] = $signup_data['signup_date'];
+        $item[] = $signup_data['tag'];
+
+        $csv[] = implode(',', $item);
+
+    }
+
+}
+
 $content = implode("\n", $csv);
 $content = mb_convert_encoding($content, 'Big5');
 
