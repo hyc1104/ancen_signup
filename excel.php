8d9d82d1b4a3327c3f5751cb5fa43ebe9e4f66fc
Ancen <ancen@mail.clhyc.tw>
2021-10-31 16:35:31 +0800

    7-5-4 產生完整的 Excel 匯出檔

--- excel.php
+++ excel.php
@@ -1,7 +1,7 @@
 <?php
 use Xmf\Request;
 use XoopsModules\Ancen_signup\Ancen_signup_actions;
-use XoopsModules\Tadtools\Utility;
+use XoopsModules\Ancen_signup\Ancen_signup_data;
 /*-----------引入檔案區--------------*/
 require_once __DIR__ . '/header.php';
 
@@ -41,13 +41,43 @@ foreach ($head_row as $head_data) {
 $head[] = '錄取';
 $head[] = '報名日期';
 $head[] = '身份';
-
+$row = 1;
 foreach ($head as $column => $value) {
-    $objActSheet->setCellValueByColumnAndRow($column, 1, $value); //直欄從0開始，橫列從1開始
+    $objActSheet->setCellValueByColumnAndRow($column, $row, $value); //直欄從0開始，橫列從1開始
 
 }
 
-$title = (_CHARSET === 'UTF-8') ? iconv('UTF-8', 'Big5', $title) : $title;
+// 抓出內容
+
+if ($type == 'signup') {
+    $signup = Ancen_signup_data::get_all($action['id']);
+
+    foreach ($signup as $signup_data) {
+        $item = [];
+        foreach ($signup_data['tdc'] as $user_data) {
+            $item[] = implode('|', $user_data);
+        }
+
+        if ($signup_data['accept'] === '1') {
+            $item[] = '錄取';
+        } elseif ($signup_data['accept'] === '0') {
+            $item[] = '未錄取';
+        } else {
+            $item[] = '尚未設定';
+        }
+
+        $item[] = $signup_data['signup_date'];
+        $item[] = $signup_data['tag'];
+
+        $row++;
+        foreach ($item as $column => $value) {
+            $objActSheet->setCellValueByColumnAndRow($column, $row, $value); //直欄從0開始，橫列從1開始
+
+        }
+    }
+}
+
+//$title = (_CHARSET === 'UTF-8') ? iconv('UTF-8', 'Big5', $title) : $title;
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header("Content-Disposition: attachment;filename={$title}.xlsx");
 header('Cache-Control: max-age=0');
