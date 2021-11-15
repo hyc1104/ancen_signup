2a718376ef8c0405fd387a7ce7ed48692e273009
Ancen <ancen@mail.clhyc.tw>
2021-11-15 13:59:44 +0800

    9-5-2 建立英文語系

--- language/english/admin.php
+++ language/english/admin.php
@@ -1,2 +1,10 @@
 <?php
 xoops_loadLanguage('admin_common', 'tadtools');
+
+// admin/main.php
+define('_MA_ANCEN_SIGNUP_DELETE_SUCCESS', 'Successfully deleted activity!');
+
+//admin/power.php
+define('_MA_ANCEN_SIGNUP_CREATE_ACTION', 'Create enrollment activity');
+define('_MA_ANCEN_SIGNUP_DETAIL_PERMISSION_SETTING', 'Detail permission setting');
+define('_MA_ANCEN_SIGNUP_CHECK_PERMISSIONS', 'Please check the permissions you want to open to the group:');
--- language/english/modinfo.php
+++ language/english/modinfo.php
@@ -1,5 +1,22 @@
 <?php
 xoops_loadLanguage('modinfo_common', 'tadtools');
 
-define("_MI_XXX_ADMENU1", "Main");
-define("_MI_XXX_ADMENU1_DESC", "Admin main page");
+// xoops_version.php
+define('_MI_ANCEN_SIGNUP_NAME', 'Event Registration');
+define('_MI_ANCEN_SIGNUP_DESCRIPTION', 'Event Registration Module');
+define('_MI_ANCEN_SIGNUP_AUTHOR_WEBSITE_NAME', 'CLHYC');
+define('_MI_ANCEN_SIGNUP_TEMPLATES_ADMIN', 'Backend Common Template');
+define('_MI_ANCEN_SIGNUP_TEMPLATES_INDEX', 'Common front-end template');
+define('_MI_ANCEN_SIGNUP_ACTION_LIST_NAME', 'List of reportable events');
+define('_MI_ANCEN_SIGNUP_ACTION_LIST_DESCRIPTION', 'List of all registerable events');
+define('_MI_ANCEN_SIGNUP_ACTION_ACTION_SIGNUP_NAME', 'Event registration focus');
+define('_MI_ANCEN_SIGNUP_ACTION_ACTION_SIGNUP_DESCRIPTION', 'The ability to select an event for users to sign up for');
+
+define('_MI_ANCEN_SIGNUP_SHOW_NUMBER', 'Show the number of events per page');
+define('_MI_ANCEN_SIGNUP_SHOW_NUMBER_DESC', 'Show the number of events per page as a basis for paging');
+define('_MI_ANCEN_SIGNUP_ONLY_ENABLE', 'Whether to show only enrolable events');
+define('_MI_ANCEN_SIGNUP_ONLY_ENABLE_DESC', 'If only enrolable events will be shown, otherwise unenrollable events will be listed');
+
+// admin/menu.php
+define('_MI_ANCEN_SIGNUP_ACCTIONS_MANAGER', 'Event Management');
+define('_MI_ANCEN_SIGNUP_PERMISSION_SETTING', 'permission setting');
